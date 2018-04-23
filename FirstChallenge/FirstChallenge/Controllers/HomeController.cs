790426686c5c2d6c25a3e18ca335bb64ec0c17eb
MGH <miriamhall.mgh@gmail.com>
2018-04-23 12:52:11 -0700

    Initial commit

---
+++ FirstChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comicBooks = ComicBookManager.GetComicBooks();
+            var comic = comicBooks.FirstOrDefault(p => p.ComicBookId == id);
+            return View(comic);
+        }
+    }
+}
